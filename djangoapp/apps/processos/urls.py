d42c8bcec6f10e440eb9efd55e7cc2595d0de291
pedro <pedro.abarboza@gmail.com>
2024-06-01 02:41:18 -0300

    Inserindo modulo de serviços - 1

--- djangoapp/apps/processos/urls.py
+++ djangoapp/apps/processos/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('delete_processos/<int:pk>', DelProcessos.as_view(), name='delete_processos'),
     
     path('listagem_reclamada', ListagemReclamadas.as_view(), name='listagem_reclamada'),
+    path('autocomplete_reclamada', AutoCompReclamadas.as_view(), name='autocomplete_reclamada'),
 
     path('autocomplete_reclamante', AutoCompReclamantes.as_view(), name='autocomplete_reclamante'),
     path('listagem_reclamante', ListagemReclamantes.as_view(), name='listagem_reclamante'),
